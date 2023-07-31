61e2bc856c845b947661eaaa3cee132205831fa1
Nick <kalinovne.io@gmail.com>
2023-07-31 14:15:11 +0900

    Commited tasks 1-3

---
+++ partnership/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/partnership/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/partnership/howtopartner.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partnership/timetable/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
